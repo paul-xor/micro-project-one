fd4a06baa268a7a033343083a1f4bafdb96a5f08
Pavel Smolov <plexsmol@gmail.com>
2022-11-23 19:03:43 -0500

    feat: add building post submission and handling CORS

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,11 @@
 import express, { json } from 'express'
 import { randomBytes } from 'crypto'
+import cors from 'cors'
 
 
 const app = express()
 app.use(json())
+app.use(cors())
 
 const commentsByPostId = {}
 
