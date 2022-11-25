9e76031a261a65bce591314b0f20d552100817dd
Pasha <psmolov@nautilusdigital.com>
2022-11-25 18:41:37 -0500

    feat: add query service

---
+++ query/index.js
@@ -0,0 +1,38 @@
+import express, { json } from 'express'
+import cors from 'cors'
+
+const app = express();
+
+app.use(json())
+app.use(cors())
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log('🚀 Server-query is running on port: 4002')
+})
