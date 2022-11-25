c77040193a83eeb5f2fda2b7916b763c67368908
Pasha <psmolov@nautilusdigital.com>
2022-11-25 17:45:15 -0500

    feat: recieving events

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,13 +7,19 @@ const app = express()
 app.use(json())
 app.use(cors())
 
-app.post('/event', (req, res) => {
-  const event = req.body
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-  axios.post('http://localhost:4000/events', event)
-  axios.post('http://localhost:4001/events', event)
-  axios.post('http://localhost:4002/events', event)
-  res.send({ status: 'OK' })
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
 })
 
 app.listen(4005, () => {
