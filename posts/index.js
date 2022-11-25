9caa080d1a5936f6106f82ff0adb95008ebda44f
Pasha <psmolov@nautilusdigital.com>
2022-11-25 15:57:13 -0500

    feat: emmiting comment creation event

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express'
 import { randomBytes } from 'crypto'
-import cors from 'cors';
+import cors from 'cors'
+import axios from 'axios'
+
 
 const app = express()
 
@@ -13,10 +15,17 @@ app.get('/posts', (req, res) => {
   res.send(posts)
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async(req, res) => {
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
   posts[id] = { id, title }
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id, title
+    }
+  })
   res.status(201).send(posts[id])
 })
 
