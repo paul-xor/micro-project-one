b385df756e111056837e5bd599e017d2f9735720
Pasha <psmolov@nautilusdigital.com>
2022-11-29 18:23:01 -0500

    feat: updating comment content

--- comments/index.js
+++ comments/index.js
@@ -36,11 +36,33 @@ app.post('/posts/:id/comments', async(req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Received Event!', req.body.type)
+app.post("/events", async (req, res) => {
+  console.log("Event Received:", req.body.type);
 
-  res.send({})
-})
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
+});
 
 app.listen(4001, () => {
   console.log('🚀 Server running on PORT: 4001')
