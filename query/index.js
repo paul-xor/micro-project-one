b6074cf5ef0c99a4566c2761154a5565b2c0b82a
Pasha <psmolov@nautilusdigital.com>
2022-11-30 18:24:12 -0500

    feat: implementing even sync

--- query/index.js
+++ query/index.js
@@ -8,13 +8,7 @@ app.use(cors())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
 
@@ -28,7 +22,7 @@ app.post('/events', (req, res) => {
     post.comments.push({ id, content, status });
   }
 
-  if(type === 'CommentUpdated') {
+  if (type === 'CommentUpdated') {
     const { id, content, postId, status } = data
     const post = posts[postId]
     const comment = post.comments.find(comment => {
@@ -37,12 +31,31 @@ app.post('/events', (req, res) => {
     comment.status = status
     comment.content = content
   }
+}
 
-  console.log(posts);
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data)
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async() => {
   console.log('🚀 Server-query is running on port: 4002')
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 })
